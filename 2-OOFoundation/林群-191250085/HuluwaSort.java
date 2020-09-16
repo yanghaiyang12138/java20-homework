2cff5507984f846cd059425953d1ad8c3e082542
Caserta-012 <2396813830@qq.com>
2020-09-16 19:34:22 +0800

    homework02

--- 2-OOFoundation/林群-191250085/HuluwaSort.java
+++ 2-OOFoundation/林群-191250085/HuluwaSort.java
@@ -1,91 +1,36 @@
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.util.HashSet;
+import java.util.Random;
+
 public class HuluwaSort {
-    static class Huluwa{
-        private int order;//葫芦娃排名
 
-        Huluwa(int order) {
-            this.order = order;
-        }
+    //初始化队列，随机站队
+    static void initialize(Huluwa[] queue) {
+        Random rand = new Random();
+        HashSet<Integer> hashSet = new HashSet();
+        int i = 0;
 
-        void setOrder(int order){this.order = order;}
-        int getOrder(){return this.order;}
-
-        //报数
-        void number(){
-            switch (this.order){
-                case 1:
-                    System.out.print("老大 ");
-                    break;
-                case 2:
-                    System.out.print("老二 ");
-                    break;
-                case 3:
-                    System.out.print("老三 ");
-                    break;
-                case 4:
-                    System.out.print("老四 ");
-                    break;
-                case 5:
-                    System.out.print("老五 ");
-                    break;
-                case 6:
-                    System.out.print("老六 ");
-                    break;
-                case 7:
-                    System.out.print("老七 ");
-                    break;
-                default:
-            }
-        }
-
-        boolean compare(Huluwa huluwa) {
-            return this.order < huluwa.getOrder();
-        }
-
-        void swap(Huluwa huluwa) {
-            int temp = huluwa.getOrder();
-            huluwa.setOrder(this.order);
-            this.setOrder(temp);
-        }
-    }
-
-    static void orchestration(Huluwa[] queue) {
-        Huluwa[] newqueue = new Huluwa[7];
-        for (Huluwa huluwa : queue) {
-            int order = huluwa.getOrder();
-            newqueue[order - 1] = huluwa;
-        }
-
-        for (Huluwa huluwa : newqueue) {
-            huluwa.number();
-        }
-    }
-
-    static void choreography(Huluwa[] queue) {
-        int length = queue.length;
-
-        //bubble sort
-        for (int i = 0;i < length-1;i++){
-            for (int j = 0;j < length-i-1;j++){
-                if (queue[j+1].compare(queue[j])){
-                    queue[j].swap(queue[j+1]);
-                }
-            }
-        }
-
-        for (Huluwa huluwa : queue) {
-            huluwa.number();
+        while (hashSet.size() < 7){
+            int tmp = rand.nextInt(7) + 1;
+            if (!hashSet.contains(tmp))queue[i++] = new Huluwa(tmp);
+            hashSet.add(tmp);
         }
     }
 
     public static void main(String[] args) {
-        int[] number = {3,6,7,5,2,4,1}; //随意站队
         Huluwa[] queue = new Huluwa[7];
-        for (int i = 0;i < 7;i++){
-            queue[i] = new Huluwa(number[i]);
-        }
+        GrandPa grandPa = new GrandPa();
 
-        orchestration(queue);
-        System.out.println("");
-        choreography(queue);
+
+
+        initialize(queue);
+        System.out.println("Orchestration:");
+        BubbleSort.orchestration(queue,grandPa);
+
+        initialize(queue);
+        System.out.println("\nChoreography:");
+        BubbleSort.choreography(queue);
+
     }
 }
