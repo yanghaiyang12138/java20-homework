3b7dcc2c51dad91bff240b22895a7d2adc437888
polarstormx <1204139878@qq.com>
2020-09-19 18:50:47 +0800

    finish README

--- 2-OOFoundation/王一之-171860550/HuluSort/src/HuLuWa.java
+++ 2-OOFoundation/王一之-171860550/HuluSort/src/HuLuWa.java
@@ -1,12 +1,15 @@
-public class HuLuWa {
-    enum HuluColor {
-        RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPUL
+public class HuLuWa extends Unit {
+    enum HuLuColor {
+        RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE
     }
-    int rank;
-    HuluColor color;
-    public HuLuWa(int rank, HuluColor color) {
+
+    final int rank;
+    final HuLuColor color;
+
+    public HuLuWa(int rank, HuLuColor color) {
         this.rank = rank;
         this.color = color;
+        this.pos = -1;//无位置
     }
 
     public boolean compareRank(HuLuWa hulu) {
--- 2-OOFoundation/王一之-171860550/HuluSort/src/LineUp.java
+++ 2-OOFoundation/王一之-171860550/HuluSort/src/LineUp.java
@@ -1,51 +1,54 @@
 import java.util.Random;
+
 public class LineUp {
-    public static void sortChoreography(Ground []g) {//协作
-        boolean flag=false;
-        while(!flag){//若进行了位置交换则继续
-            flag=true;
-            for(int i = 0; i <g.length-1; ++i){//每个身后有人的葫芦娃看自己是否需要和身后交换位置
-                if(g[i].getHulu().compareRank(g[i+1].getHulu())){
-                    flag=false;
-                    Ground.swap(g[i],g[i+1]);//交换位置
+    public static void sortChoreography(Tile[] g, HuLuWa[] hulu) {//协作
+        boolean flag = false;
+        while (!flag) {//若进行了位置交换则继续
+            flag = true;
+            for (HuLuWa h : hulu) {//每个身后有人的葫芦娃看自己是否需要和身后交换位置
+                if (h.pos < g.length - 1 && h.compareRank((HuLuWa) g[h.pos + 1].getMember())) {
+                    flag = false;
+                    Tile.swap(g[h.pos], g[h.pos + 1]);//交换位置
                 }
             }
         }
     }
-    public static void sortOrchestration(Ground []g) {//指挥
+
+    public static void sortOrchestration(Tile[] g) {//指挥 冒泡
         for (int i = 1; i < g.length; ++i)
             for (int j = 0; j < g.length - i; ++j) {
-                if (g[j].getHulu().rank<g[j+1].getHulu().rank) {
-                    Ground.swap(g[j],g[j+1]);
+                if (((HuLuWa) g[j].getMember()).rank < ((HuLuWa) g[j + 1].getMember()).rank) {
+                    Tile.swap(g[j], g[j + 1]);
                 }
             }
     }
-    public static void randomInit(Ground []g,HuLuWa[]h) {
-        if (h.length!=g.length)
+
+    public static void randomInit(Tile[] g, HuLuWa[] h) {
+        if (h.length != g.length)
             return;//error
-        int len=g.length;
-        int[] array=new int[len];
+        int len = g.length;
+        int[] array = new int[len];
         for (int i = 0; i < len; ++i) {
             array[i] = 0;
         }
         int pos;
         Random r = new Random();
         for (int i = 0; i < len; ++i) {//生成随机序列
-            do{
-                pos = r.nextInt(len-1);
-            }while (array[pos] != 0);
+            do {
+                pos = r.nextInt(len - 1);
+            } while (array[pos] != 0);
             array[pos] = i;
         }
 
         for (int i = 0; i < len; ++i) {//用该序列初始化位置
-            g[i]=new Ground(h[array[i]]);
+            g[i] = new Tile(i, h[array[i]]);
         }
     }
 
-    public static void printHulu(Ground []g) {
-        for (Ground ground : g) {
-            if (!ground.isEmpty)
-                System.out.println(ground.getHulu().rank);
+    public static void printTile(Tile[] ground) {
+        for (Tile g : ground) {
+            if (!g.isEmpty)
+                System.out.println(((HuLuWa) g.getMember()).rank);
             else
                 System.out.println("此处为空");
         }
@@ -53,28 +56,28 @@ public class LineUp {
     }
 
     public static void main(String[] args) {
-        Ground []grounds = new Ground[7];//生成7块空地
+        Tile[] ground = new Tile[7];//生成7块空地
 
         HuLuWa[] hulu = new HuLuWa[7];
-        hulu[0] = new HuLuWa(1, HuLuWa.HuluColor.RED);
-        hulu[1] = new HuLuWa(2, HuLuWa.HuluColor.ORANGE);
-        hulu[2] = new HuLuWa(3, HuLuWa.HuluColor.YELLOW);
-        hulu[3] = new HuLuWa(4, HuLuWa.HuluColor.GREEN);
-        hulu[4] = new HuLuWa(5, HuLuWa.HuluColor.CYAN);
-        hulu[5] = new HuLuWa(6, HuLuWa.HuluColor.BLUE);
-        hulu[6] = new HuLuWa(7, HuLuWa.HuluColor.PURPUL);
+        hulu[0] = new HuLuWa(1, HuLuWa.HuLuColor.RED);
+        hulu[1] = new HuLuWa(2, HuLuWa.HuLuColor.ORANGE);
+        hulu[2] = new HuLuWa(3, HuLuWa.HuLuColor.YELLOW);
+        hulu[3] = new HuLuWa(4, HuLuWa.HuLuColor.GREEN);
+        hulu[4] = new HuLuWa(5, HuLuWa.HuLuColor.CYAN);
+        hulu[5] = new HuLuWa(6, HuLuWa.HuLuColor.BLUE);
+        hulu[6] = new HuLuWa(7, HuLuWa.HuLuColor.PURPLE);
 
-        randomInit(grounds,hulu);
+        randomInit(ground, hulu);
         System.out.println("init:");
-        printHulu(grounds);
+        printTile(ground);
 
-        sortOrchestration(grounds);
+        sortOrchestration(ground);
         System.out.println("after Orchestration:");
-        printHulu(grounds);
+        printTile(ground);
 
-        sortChoreography(grounds);
+        sortChoreography(ground, hulu);
         System.out.println("after Choreography:");
-        printHulu(grounds);
+        printTile(ground);
 
     }
 
