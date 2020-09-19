8adf034d9ff5db2eae9802190eb970d5d3764f8b
sevenxqq <fantastic772256@163.com>
2020-09-20 00:01:33 +0800

    fin choWorld

--- 2-OOFoundation/徐佳美-181860117/OrchestrationWorld.java
+++ 2-OOFoundation/徐佳美-181860117/OrchestrationWorld.java
@@ -5,10 +5,11 @@ public class OrchestrationWorld {
     public static void main(String[] args) {
         Grandpa grandpa = new Grandpa();
         Calabash grandsons[] = grandpa.createCalabash();
+        System.out.println("OrchestrationWorld");
         grandpa.randLine(grandsons);
         System.out.println("排序前  ");
         grandpa.report(grandsons);
-        grandpa.bubbleSort(grandsons);
+        grandpa.newSort(grandsons);
         System.out.println("排序后  ");
         grandpa.report(grandsons);
 
@@ -53,9 +54,10 @@ class Grandpa {
         grandsons[b] = temp;
     }
 
-    private static Random rand = new Random();
+    
 
     public void randLine(Calabash[] grandsons) {
+        Random rand = new Random();
         int len = grandsons.length;
         for (int i = len; i > 0; i--) {
             int randInd = rand.nextInt(i);
@@ -71,7 +73,7 @@ class Grandpa {
         System.out.println();
     }
 
-    public void bubbleSort(Calabash[] grandsons) {
+    public void newSort(Calabash[] grandsons) {
         int len = grandsons.length;
         for (int i = 0; i < len; i++) {
             for (int j = i + 1; j < len; j++) {
